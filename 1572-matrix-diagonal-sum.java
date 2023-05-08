ea4e286bfbeefcd591d555583ad7b832bb8f4f46
manishjhacse <jhamanish.cse25@gmail.com>
2023-05-08 15:22:35 +0530

    first commit

---
+++ 1572-matrix-diagonal-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+      int sum=0;
+      int n=mat.length;
+      for(int i=0;i<n;i++)
+      {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j || i+j==n-1)
+            {
+                sum=sum+mat[i][j];
+            
+            }
+        }
+      } 
+    return sum;
+    }
+}
