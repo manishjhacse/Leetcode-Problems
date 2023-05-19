ef09d99722776af03f066d6f9f7af8b7489d6d6d
manishjhacse <jhamanish.cse25@gmail.com>
2023-05-19 19:47:12 +0530

    first commit

---
+++ 20-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+            //Approach: using Stack
+            Stack<Character> st = new Stack<>();
+    
+            //iterate through the given string add to stack
+            for(char a:s.toCharArray()){
+                // char a = s.charAt(i);
+                if(a==\'(\' || a==\'{\' || a==\'[\'){
+                    st.push(a);
+                }
+                //remove
+               else if(a==\')\' && !st.empty() && st.peek()==\'(\') st.pop();
+                else if(a==\'}\' && !st.empty() && st.peek()==\'{\') st.pop();
+                else if(a==\']\' && !st.empty() && st.peek()==\'[\') st.pop();
+                else return false;
+            }
+            return st.empty();
+    }
+}
