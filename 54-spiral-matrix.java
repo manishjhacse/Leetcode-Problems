b4c24fe62dc2ffbaea420da2e297ae74f61346cb
manishjhacse <jhamanish.cse25@gmail.com>
2023-09-22 13:24:54 +0530

    solution of problem no. 54

---
+++ 54-spiral-matrix.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+
+    }
+}
