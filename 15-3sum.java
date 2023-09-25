0e43cdb348bb11b48321db319045d03f47b36ac4
manishjhacse <jhamanish.cse25@gmail.com>
2023-09-25 19:32:15 +0530

    solution of problem no. 15

--- 15-3sum.java
+++ 15-3sum.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        HashSet<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    set.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+
+        return ans;
     }
 }
