25054fb862a549f51f3bf9d8b25945b096c13f9f
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2023-10-12 19:42:59 +0530

    solution of problem no. 1480

---
+++ 1480-running-sum-of-1d-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            nums[i]=sum;
+        }
+        return nums;
+    }
+}
