dce2cee41e63b09b1193da97f06a1137b4137a46
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2023-10-15 13:56:01 +0530

    solution of problem no. 74

--- 74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+        int n=matrix.length;
+        int m=matrix[0].length;
         int low=0;
         int high=m*n-1;
         while(low<=high)
         {
             int mid_index=low+(high-low)/2;
-            int row=mid_index/n;
-            int col=mid_index%n;
+            int row=mid_index/m;
+            int col=mid_index%m;
             int mid_element=matrix[row][col];
             if(mid_element==target)
             {
