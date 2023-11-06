1e796e7697e088c826e12f5bfe9ce97b3aac737b
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2023-11-06 17:41:24 +0530

    solution of problem no. 160

--- 160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists.java
@@ -13,44 +13,49 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode tempA=headA;
         ListNode tempB=headB;
-        int lenA=0,lenB=0,diff;
-        while(tempA!= null)
-        {
-            lenA++;
-            tempA=tempA.next;
+        while (tempA!=tempB) {
+            tempA=tempA==null?headB:tempA.next;
+            tempB=tempB==null?headA:tempB.next;
         }
-        while(tempB!=null)
-        {
-            lenB++;
-            tempB=tempB.next;
-        }
-        tempA=headA;
-        tempB=headB;
-        if(lenA>lenB)
-        {
-            diff=lenA-lenB;
-            while (diff > 0){
-                tempA=tempA.next;
-                diff--;
-            }
-        }
-        else{
-            diff=lenB-lenA;
-             while (diff > 0){
-                tempB=tempB.next;
-                diff--;
-            }
-        }
-        while(tempA!=null && tempB!=null)
-        {
-            if(tempA==tempB)
-            {
-                return tempA;
-            }
-            tempA=tempA.next;
-            tempB=tempB.next;
-        }
-        return null;
+        return tempA;
+        // int lenA=0,lenB=0,diff;
+        // while(tempA!= null)
+        // {
+        //     lenA++;
+        //     tempA=tempA.next;
+        // }
+        // while(tempB!=null)
+        // {
+        //     lenB++;
+        //     tempB=tempB.next;
+        // }
+        // tempA=headA;
+        // tempB=headB;
+        // if(lenA>lenB)
+        // {
+        //     diff=lenA-lenB;
+        //     while (diff > 0){
+        //         tempA=tempA.next;
+        //         diff--;
+        //     }
+        // }
+        // else{
+        //     diff=lenB-lenA;
+        //      while (diff > 0){
+        //         tempB=tempB.next;
+        //         diff--;
+        //     }
+        // }
+        // while(tempA!=null && tempB!=null)
+        // {
+        //     if(tempA==tempB)
+        //     {
+        //         return tempA;
+        //     }
+        //     tempA=tempA.next;
+        //     tempB=tempB.next;
+        // }
+        // return null;
         
     }
 }
