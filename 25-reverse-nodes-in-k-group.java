9dfa32dd789e0df2dcc841bda8eae2571894b926
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2023-11-16 21:33:30 +0530

    solution of problem no. 25

--- 25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group.java
@@ -1,27 +1,39 @@
+import java.util.List;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        int size=0;
-        ListNode temp=head;
-        while(head!=null)
-        {
+        if (head == null || k == 1)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy, nex = dummy, prev = dummy;
+        int size = 0;
+        while (curr.next != null) {
             size++;
-            temp=temp.next;
+            curr = curr.next;
         }
-        temp=head;
-        if(size%k==0)
-        {
-
+        while (size >= k) {
+            curr = prev.next;
+            nex = curr.next;
+            for (int i = 1; i < k; i++) {
+                curr.next = nex.next;
+                nex.next = prev.next;
+                prev.next = nex;
+                nex = curr.next;
+            }
+            prev = curr;
+            size -= k;
         }
-
+        return dummy.next;
     }
 }
