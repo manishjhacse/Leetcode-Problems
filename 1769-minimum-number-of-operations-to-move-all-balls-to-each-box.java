5be539b934d9c94364419e80061b94101844d28c
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2023-12-03 20:09:01 +0530

    solution of problem no. 1769

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int[] minOperations(String boxes){
+        int n = boxes.length();
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            int t = 0;
+            for(int j=0; j<n; j++){
+                char c = boxes.charAt(j);
+                if(c=='1') t += Math.abs(i-j);
+            }
+            ans[i] = t;
+        }
+        return ans;
+    }
+}
