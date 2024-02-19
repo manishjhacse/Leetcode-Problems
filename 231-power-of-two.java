598ac358470d350546abb43da2cb1ee3bdfc1572
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-02-19 18:28:35 +0530

    solution of problem no. 231

---
+++ 231-power-of-two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n < 1) {
+            return false;
+        }
+
+        while (n >= 2) {
+            if ((n % 2 != 0 && n != 1)) {
+                return false;
+            }
+
+            n = n / 2;
+        }
+
+        return true;
+    }
+}
