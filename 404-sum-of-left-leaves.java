8ac22cf1f85b2838714a7674c0c3612bffebd424
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-04-14 14:41:13 +0530

    solution of POTD 14-04-2024

---
+++ 404-sum-of-left-leaves.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int returnSum(TreeNode root, boolean isLeft) {
+        if(root==null){
+            return 0;
+        }
+        if(root.left==null && root.right==null){
+            return isLeft?root.val:0; 
+        }
+        int count=0;
+        count+= returnSum(root.left,true);
+        count+=returnSum(root.right,false);
+        return count;
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+      return returnSum(root,false);
+    }
+}
