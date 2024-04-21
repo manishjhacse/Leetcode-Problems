17575e48f94fe6302876d06d90cdd14b7926a04f
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-04-21 19:04:20 +0530

    solution of POTD 21-04-2024

---
+++ 1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        queue.offer(source);
+        visited[source] = true;
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (current == destination) {
+                return true;
+            }
+            for (int neighbor : adjacencyList.get(current)) {
+                if (!visited[neighbor]) {
+                    queue.offer(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
