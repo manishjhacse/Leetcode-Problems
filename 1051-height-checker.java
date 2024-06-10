39a6ca77604e285ad367f090c7b4d20e5e3f6bd5
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-06-10 21:56:39 +0530

    solution of POTD 10-06-2024

--- 1051-height-checker.java
+++ 1051-height-checker.java
@@ -1,19 +1,16 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int temp[] = new int[heights.length];
-        for(int i=0;i<temp.length;i++)
-        {
-            temp[i]=heights[i];
+       int arr[]=new int[heights.length];
+       for(int i=0;i<arr.length;i++){
+        arr[i]=heights[i];
+       }
+       Arrays.sort(heights);
+       int count=0;
+       for(int i=0;i<heights.length;i++){
+        if(heights[i]!=arr[i]){
+            count++;
         }
-        Arrays.sort(heights);
-        int count=0;
-        for(int i=0;i<heights.length;i++)
-        {
-            if(heights[i]!=temp[i])
-            {
-                count++;
-            }
-        }
-        return count;
+       }
+       return count;
     }
 }
