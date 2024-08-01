e7d40a20f9b226858e2aca40ce2b25546c52c19b
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-08-01 15:28:46 +0530

    solution of POTD 01-08-2024

---
+++ 2678-number-of-senior-citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int seniors = 0;
+        for (int i = 0; i < details.length; i++) {
+            int age = (details[i].charAt(11) - '0') * 10 + (details[i].charAt(12) - '0');
+            if (age > 60)
+                seniors++;
+        }
+        return seniors;
+    }
+}
