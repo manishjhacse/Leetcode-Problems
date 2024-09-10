9159089eb766683d098f4ba4ec74a9f1660828fe
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-09-10 20:46:24 +0530

    Priority Queue

--- 857-minimum-cost-to-hire-k-workers.java
+++ 857-minimum-cost-to-hire-k-workers.java
@@ -1,37 +1,47 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 class Solution {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
         int n = quality.length;
-        double minCost = Double.MAX_VALUE;
-        Worker[] workers = new Worker[n];
+        ArrayList<double[]> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            workers[i] = new Worker(quality[i], wage[i]);
+            double ratio = 1.0 * wage[i] / quality[i];
+            list.add(new double[] { ratio, quality[i] });
         }
-        Arrays.sort(workers, (a, b) -> Double.compare(a.wageRatio(), b.wageRatio()));
-        PriorityQueue<Integer> maxQualityHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Collections.sort(list, new Comparator<double[]>() {
+            public int compare(double a[], double b[]) {
+                if (a[0] < b[0]) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        PriorityQueue<double[]> pq = new PriorityQueue<>(new Comparator<double[]>() {
+            public int compare(double a[], double b[]) {
+                if (a[1] < b[1]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
         int totalQuality = 0;
-        for (Worker worker : workers) {
-            maxQualityHeap.offer(worker.quality);
-            totalQuality += worker.quality;
-            if (maxQualityHeap.size() > k) {
-                totalQuality -= maxQualityHeap.poll();
-            }
-            if (maxQualityHeap.size() == k) {
-                minCost = Math.min(minCost, totalQuality * worker.wageRatio());
+        double minCost = Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            double arr[] = list.get(i);
+            pq.offer(arr);
+            totalQuality += arr[1];
+            if (pq.size() == k) {
+                double minWage = arr[0];
+                minCost = Math.min(minCost, minWage * totalQuality);
+                totalQuality -= pq.poll()[1];
+
             }
         }
-    
         return minCost;
     }
-    
-    class Worker {
-        int quality;
-        int wage;
-        public Worker(int quality, int wage) {
-            this.quality = quality;
-            this.wage = wage;
-        }
-        public double wageRatio() {
-            return (double) wage / quality;
-        }
-    }
+
 }
