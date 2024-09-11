9aa6b321ef8fc6a8cccefd48ec99b7e3facc44c4
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-09-11 20:20:44 +0530

    PQ question

--- 826-most-profit-assigning-work.java
+++ 826-most-profit-assigning-work.java
@@ -1,29 +1,31 @@
 class Solution {
-    int maxValue(int difficulty[]) {
-        int max = Integer.MIN_VALUE;
-        for (int num : difficulty) {
-            if (num > max)
-                max = num;
+    private int maxDifficulty(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
         }
         return max;
     }
 
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int maxProfit = 0;
-        int jobs[] = new int[maxValue(difficulty) + 1];
+        int maxValue = maxDifficulty(difficulty);
+        int jobs[] = new int[maxValue + 1];
         for (int i = 0; i < difficulty.length; i++) {
             jobs[difficulty[i]] = Math.max(jobs[difficulty[i]], profit[i]);
         }
         for (int i = 1; i < jobs.length; i++) {
             jobs[i] = Math.max(jobs[i], jobs[i - 1]);
         }
+        int maxProfit = 0;
         for (int i = 0; i < worker.length; i++) {
-            if (worker[i] >= jobs.length) {
-                maxProfit += jobs[jobs.length - 1];
+            if (worker[i] > maxValue) {
+                maxProfit += jobs[maxValue];
             } else {
                 maxProfit += jobs[worker[i]];
             }
         }
         return maxProfit;
+
     }
 }
