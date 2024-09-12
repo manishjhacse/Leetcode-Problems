ca5860499fa2df11544b513a0399895521daf9ad
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-09-12 19:08:08 +0530

    solution of POTD 12-09-2024

--- 1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings.java
@@ -1,27 +1,24 @@
+import java.util.HashSet;
+
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        HashSet<Character> set = new HashSet<>();
-        
-        int ans =0;
-        for(int i =0;i<allowed.length();i++){
-            set.add(allowed.charAt(i));
+        int arr[]=new int[26];
+        int consistent = 0;
+        for (char c : allowed.toCharArray()) {
+            arr[c-'a']++;
         }
-        for(int i =0;i<words.length;i++){
-            int count =0;
-            for(int j =0;j<words[i].length();j++){
-            if(!set.contains(words[i].charAt(j)))
-            count ++;
-           
-            
-            
-        }
-         if(count==0){
-                ans ++;
+        for (int i = 0; i < words.length; i++) {
+            boolean flag = true;
+            for (char c : words[i].toCharArray()) {
+                if (arr[c-'a']==0) {
+                    flag = false;
+                    break;
+                }
             }
-    
-    
-      
+            if (flag) {
+                consistent++;
+            }
+        }
+        return consistent;
     }
-    return ans;
-}
 }
