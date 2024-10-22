87f6578ec0e7cbfd84520ef5f00fee03aac4707f
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-10-22 23:45:19 +0530

    solution of POTD 22-10-2024

---
+++ 2583-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    long[] level_sum = new long[100001];
+    int max_ind = -1;
+    void dfs(TreeNode root, int level) {
+        if(root == null) return;
+        max_ind = Math.max(max_ind, level);
+        level_sum[level] += root.val;
+
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        dfs(root, 0);
+        max_ind++;
+        if(max_ind < k) return -1;
+        Arrays.sort(level_sum, 0, max_ind);
+        return level_sum[max_ind - k];
+    }
+}
